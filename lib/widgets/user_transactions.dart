68198d7eced6d9febf58fcdc77c4623af02eaa4b
Kalam031 <abukalam973@gmail.com>
2020-09-25 11:50:10 +0600

    Creating Scrollable List view

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -22,6 +22,42 @@ class _UserTransactionsState extends State<UserTransactions> {
       amount: 16.53,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
