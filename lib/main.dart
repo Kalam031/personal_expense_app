c888698a4a50eaa8b79a0ce384bc3be02eb055d9
Kalam031 <abukalam973@gmail.com>
2020-10-16 20:24:12 +0600

    adding layout builder widget

--- lib/main.dart
+++ lib/main.dart
@@ -139,7 +139,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.4,
+                  0.3,
               child: Chart(
                 _recentTransactions,
               ),
@@ -148,7 +148,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
               child: TransactionList(
                 _userTransactions,
                 _deleteTransaction,
